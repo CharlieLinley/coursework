a2db681ecdb8ef9cde5944936f9b9be16ee48776
CharlieLinley <charlielinley15@gmail.com>
2023-07-13 18:36:44 +0100

    Added Ball Class and Created Gravity Function

--- currentgame.py
+++ currentgame.py
@@ -12,42 +12,61 @@ pygame.display.set_caption("Golf")
 class player_arrow(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("C:/Users/Charlie/Desktop/School/Coursework/latest/arrow.png").convert_alpha() #Full path (//CFBS-SVR-FILE1/PupilsData/2017/Linleyc/RedirectedProfileFolders/Desktop/Game Files/)
+        self.image = pygame.image.load('C:/Users/Charlie/Desktop/Coursework/coursework/arrow.png').convert_alpha()
         self.clean_image = self.image.copy()
-        self.rect = self.image.get_rect(center = (400,400))
         self.current_angle = 0
+        self.rect = self.image.get_rect(center = (400,400))
     
-
     def center_rotate(self, angle):
         self.current_angle += angle
-        print(self.current_angle)
         if self.current_angle < 0:
             self.current_angle = 0
         elif self.current_angle > 180:
             self.current_angle = 180
         center_loc = self.rect.center #Get center of image before rotation
-        print(center_loc)
         rot_sprite = pygame.transform.rotate(self.clean_image, self.current_angle) #Rotating image by given angle.
         self.rect = rot_sprite.get_rect(center = center_loc)
         self.image = rot_sprite
-    
-    def player_controls(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            print('Up')
-            self.center_rotate(5)
-        if keys[pygame.K_DOWN]:
-            print('Down')
-            self.center_rotate(-5)
+        
 
     def update(self):
-        self.player_controls()
+        #Set rect center to ball current center
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.center_rotate(1)
+        if keys[pygame.K_DOWN]:
+            self.center_rotate(-1)
+        screen.blit(self.image, (self.posX, self.posY))
+
+class ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.posX = 400
+        self.posY = 700
+        self.jumping = False
+        self.image = pygame.image.load("C:/Users/Charlie/Desktop/Coursework/coursework/ball.png")
+    
+    def gravity(self):
+        self.posY += 1
+        if self.posY > 700:
+            self.posY = 700
+            self.jumping = False
+    
+    def update(self):
+        keys = pygame.key.get_pressed()
+        for key in keys:
+           if keys[pygame.K_SPACE] and (not self.jumping):
+               self.posY -= 100
+               self.jumping = True
+        screen.blit(self.image, (self.posX, self.posY))
 
 
 #grouping sprites for easy updating
 game_sprites = pygame.sprite.Group()
 Arrow = player_arrow()
+Ball = ball()
 game_sprites.add(Arrow)
+game_sprites.add(Ball)
 
 def main_game():
     clock = pygame.time.Clock()   ## Sync fps with timer 
@@ -59,7 +78,6 @@ def main_game():
                 running = False #Ends game loop
         game_sprites.update() #updating all game sprites
         screen.fill(BLUE)
-        game_sprites.draw(screen)
         pygame.display.flip()
 
 main_game()
