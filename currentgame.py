0925a1b0c2de79735fcd9b0a991e7f50ce834dc9
CharlieLinley <charlielinley15@gmail.com>
2023-09-11 14:17:54 +0100

    Removed gravity function and began work on projectile motion function. Ball sprite is now a pygame circle and not an image.
    Signed-off-by: CharlieLinley <charlielinley15@gmail.com>

--- currentgame.py
+++ currentgame.py
@@ -5,17 +5,18 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 FPS = 60
 BLUE = (0,0,255)
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Golf")
 
+arrow_image = pygame.image.load('C:/Users/Charlie/Desktop/Coursework/coursework/arrow.png').convert_alpha()
+arrow_image = pygame.transform.scale(arrow_image, (100,100))
 class player_arrow(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('C:/Users/Charlie/Desktop/Coursework/coursework/arrow.png').convert_alpha()
+        self.image = arrow_image
         self.clean_image = self.image.copy()
         self.current_angle = 0
-        self.rect = self.image.get_rect(center = (400,400))
+        self.rect = self.image.get_rect(center = (600,600))
     
     def center_rotate(self, angle):
         self.current_angle += angle
@@ -41,25 +42,31 @@ class player_arrow(pygame.sprite.Sprite):
 class ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.radius = 10
         self.posX = 400
         self.posY = 700
-        self.jumping = False
-        self.image = pygame.image.load("C:/Users/Charlie/Desktop/Coursework/coursework/ball.png")
+        #self.jumping = False
     
-    def gravity(self):
-        self.posY += 1
-        if self.posY > 700:
-            self.posY = 700
-            self.jumping = False
+    # def gravity(self):
+    #     self.posY += 1
+    #     if self.posY > 700:
+    #         self.posY = 700
+    #         self.jumping = False
+
+    def ball_path(startx, starty, power, ang, time):
+        pass
+
     
     def update(self):
-        keys = pygame.key.get_pressed()
-        for key in keys:
-           if keys[pygame.K_SPACE] and (not self.jumping):
-               self.posY -= 100
-               self.jumping = True
-        screen.blit(self.image, (self.posX, self.posY))
-        self.gravity()
+        # keys = pygame.key.get_pressed()
+        # for key in keys:
+        #    if keys[pygame.K_SPACE] and (not self.jumping):
+        #        self.posY -= 100
+        #        self.jumping = True
+        pygame.draw.circle(screen, (255,255,255), (self.posX, self.posY), self.radius) #White (255,255,255) Outline to circle
+        pygame.draw.circle(screen, (0,0,0), (self.posX, self.posY), self.radius - 1) #Main Black (0,0,0) circle body
+        
+        #self.gravity()
 
 
 #grouping sprites for easy updating
@@ -80,7 +87,6 @@ def main_game():
         screen.fill(BLUE)
         game_sprites.update() #updating all game sprites
         pygame.display.flip()
-
 main_game()
 
 
