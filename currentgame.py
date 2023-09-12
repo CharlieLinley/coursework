ff13fc992a2eeabf60bd7b712d5aa733601c92c7
CharlieLinley <charlielinley15@gmail.com>
2023-09-12 16:40:29 +0100

    Removed the arrow function. Created new methods for the ball function, ball_path() and findAngle(), which now give simulate projectile motion for the ball (Currently only shoots once, needs to be fixed.)
    Signed-off-by: CharlieLinley <charlielinley15@gmail.com>

--- currentgame.py
+++ currentgame.py
@@ -9,36 +9,36 @@ BLUE = (0,0,255)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Golf")
 
-arrow_image = pygame.image.load('C:/Users/Charlie/Desktop/Coursework/coursework/arrow.png').convert_alpha()
-arrow_image = pygame.transform.scale(arrow_image, (100,100))
-class player_arrow(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = arrow_image
-        self.clean_image = self.image.copy()
-        self.current_angle = 0
-        self.rect = self.image.get_rect(center = (600,600))
+# arrow_image = pygame.image.load('//CFBS-SVR-FILE1/PupilsData/2017/Linleyc/RedirectedProfileFolders/Desktop/Game Files/arrow.png').convert_alpha() #C:/Users/Charlie/Desktop/Coursework/coursework/arrow.png
+# arrow_image = pygame.transform.scale(arrow_image, (100,100))
+# class player_arrow(pygame.sprite.Sprite):
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.image = arrow_image
+#         self.clean_image = self.image.copy()
+#         self.current_angle = 0
+#         self.rect = self.image.get_rect(center = (600,600))
     
-    def center_rotate(self, angle):
-        self.current_angle += angle
-        if self.current_angle < 0:
-            self.current_angle = 0
-        elif self.current_angle > 180:
-            self.current_angle = 180
-        center_loc = self.rect.center #Get center of image before rotation
-        rot_sprite = pygame.transform.rotate(self.clean_image, self.current_angle) #Rotating image by given angle.
-        self.rect = rot_sprite.get_rect(center = center_loc)
-        self.image = rot_sprite
+#     def center_rotate(self, angle):
+#         self.current_angle += angle
+#         if self.current_angle < 0:
+#             self.current_angle = 0
+#         elif self.current_angle > 180:
+#             self.current_angle = 180
+#         center_loc = self.rect.center #Get center of image before rotation
+#         rot_sprite = pygame.transform.rotate(self.clean_image, self.current_angle) #Rotating image by given angle.
+#         self.rect = rot_sprite.get_rect(center = center_loc)
+#         self.image = rot_sprite
         
 
-    def update(self):
-        #Set rect center to ball current center
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            self.center_rotate(1)
-        if keys[pygame.K_DOWN]:
-            self.center_rotate(-1)
-        screen.blit(self.image, self.rect)
+#     def update(self):
+#         #Set rect center to ball current center
+#         keys = pygame.key.get_pressed()
+#         if keys[pygame.K_UP]:
+#             self.center_rotate(1)
+#         if keys[pygame.K_DOWN]:
+#             self.center_rotate(-1)
+#         screen.blit(self.image, self.rect)
 
 class ball(pygame.sprite.Sprite):
     def __init__(self):
@@ -55,7 +55,36 @@ class ball(pygame.sprite.Sprite):
     #         self.jumping = False
 
     def ball_path(startx, starty, power, ang, time):
-        pass
+        veloX = math.cos(angle) * power
+        veloY = math.sin(angle) * power
+
+        distX = veloX * time
+        distY = veloY * time + (((-300) * (time ** 2)) / 2)
+
+        newX = round(distX + startx)
+        newY = round(starty - distY)
+
+        return(newX, newY)
+
+    
+    def findAngle(self, pos):
+        X = self.posX
+        Y = self.posY
+        try:
+            angle = math.atan((Y - pos[1]) / (X - pos[0])) #Tan Trig to find angle
+        except:
+            angle = math.pi / 2 
+        
+        if pos[1] < Y and pos[0] > X:
+            angle = abs(angle)
+        elif pos[1] < Y and pos[0] > X:
+            angle = math.pi - angle
+        elif pos[1] > Y and pos[0] > X:
+            angle = math.pi + abs(angle)
+        elif pos[1] > Y and pos[0] > X:
+            angle = (math.pi * 2) - angle
+        
+        return angle
 
     
     def update(self):
@@ -72,9 +101,9 @@ class ball(pygame.sprite.Sprite):
 
 #grouping sprites for easy updating
 game_sprites = pygame.sprite.Group()
-Arrow = player_arrow()
+#Arrow = player_arrow()
 Ball = ball()
-game_sprites.add(Arrow)
+#game_sprites.add(Arrow)
 game_sprites.add(Ball)
 
 click_posX = 0
@@ -89,6 +118,15 @@ shoot = False
 clock = pygame.time.Clock()   ## Sync fps with timer 
 running = True
 while running:
+    if shoot:
+        if Ball.posY < (800 - Ball.radius): #Ensures ball is not off screen.
+            time += 0.05
+            po = ball.ball_path(x,y,power,angle,time)
+            Ball.posX = po[0]
+            Ball.posY = po[1]
+        else:
+            shoot = False
+            Ball.posY = 790
     mouse_pos = pygame.mouse.get_pos()
     mouse_line = [(Ball.posX, Ball.posY), mouse_pos] #List with ball and mouse positions
     clock.tick(FPS) #syncs clock up to game fps
@@ -102,11 +140,12 @@ while running:
                 y = Ball.posY
                 time = 0
                 power = math.sqrt((mouse_line[1][1] - mouse_line[0][1])**2 + (mouse_line[1][0] - mouse_line[0][0])**2) #Pythagorous Thereom equation
+                angle = Ball.findAngle(mouse_pos)
+                print(angle)
                 print(power)
-
-        screen.fill(BLUE)
-        game_sprites.update() #updating all game sprites
-        pygame.display.flip()
-\
+    screen.fill(BLUE)
+    game_sprites.update() #updating all game sprites
+    pygame.display.flip()
+        
 
 pygame.quit() #Stops code, only reached once game loop has ended.
