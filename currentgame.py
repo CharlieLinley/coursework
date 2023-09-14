1674902a98ab8aff9d6cced3e46bd70d66d99d4f
CharlieLinley <charlielinley15@gmail.com>
2023-09-14 21:03:09 +0100

    Fixed bouncing and Allowed for wall bouncing aswell.
    Removed general reliance on fixed values.]
    
    Altered getImpactAngle function to calculate wall bounces aswell.
    
    Signed-off-by: CharlieLinley <charlielinley15@gmail.com>

--- currentgame.py
+++ currentgame.py
@@ -6,6 +6,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 FPS = 60
 BLUE = (0,0,255)
+yakuza_bg = pygame.image.load('C:/Users/Charlie/Desktop/Coursework/coursework/yakuza.webp')
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Golf")
 
@@ -13,13 +14,12 @@ class ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.radius = 10
-        self.posX = 400
-        self.posY = 790
+        self.posX = SCREEN_WIDTH / 2
+        self.posY = SCREEN_HEIGHT - self.radius
 
     def ball_path(self, startx, starty, power, ang, time):
         veloX = math.cos(ang) * power
         veloY = math.sin(ang) * power
-        print(veloX, veloY)
         distX = veloX * time
         distY = (veloY * time) + (0.5 * (-300) * (time ** 2))  # Add gravity effect
 
@@ -48,6 +48,14 @@ class ball(pygame.sprite.Sprite):
         
         return angle
 
+    def getImpactAngle(self, ang, power, wall = False): # Default wall value of false.
+        veloX = math.cos(ang) * power
+        if wall:
+            veloX = -veloX
+        veloY = math.sin(ang) * power 
+        impact_angle = math.atan2(veloY, veloX)  # Use atan2 to get the correct angle
+
+        return impact_angle
     
     def update(self):
         pygame.draw.circle(screen, (255,255,255), (self.posX, self.posY), self.radius) #White (255,255,255) Outline to circle
@@ -67,6 +75,8 @@ time = 0
 power = 0
 angle = 0
 shoot = False
+# bounceCalc = False #Keeps track if a Position has been stored for bounce angle calc.
+# checkReady = False #Tracks if ball pos should be checked.
 
 
 '''Main Game Loop'''
@@ -74,18 +84,49 @@ clock = pygame.time.Clock()   ## Sync fps with timer
 running = True
 while running:
     if shoot:
-        if Ball.posY <= (SCREEN_HEIGHT - Ball.radius): #Ensures ball is not off screen.
+        # if Ball.posY < (SCREEN_HEIGHT - (Ball.radius + 10)) and not checkReady:
+        #     checkReady = True
+        # if Ball.posY >= (SCREEN_HEIGHT - (Ball.radius + 10)) and not bounceCalc and checkReady:
+        #     # prevX = Ball.posX
+        #     # prevY = Ball.posY
+        #     # print(prevX, prevY)
+        #     bounceCalc = True
+        if Ball.posY <= (SCREEN_HEIGHT - Ball.radius): #Ensures ball is off the ground
             time += 0.05
             po = Ball.ball_path(x,y,power,angle,time)
             Ball.posX = po[0]
             Ball.posY = po[1]
         else:
-            shoot = False
-            Ball.posY = 790
-    if Ball.posX > (SCREEN_HEIGHT - Ball.radius):
-            Ball.posX = 800
-    elif Ball.posX < (0 - Ball.radius):
-            Ball.posX = 0
+            Ball.posY = SCREEN_HEIGHT - Ball.radius 
+            power *= 0.8  #Simulating energy lost to friction / elastic heat
+            if power > 1:
+                x = Ball.posX
+                y = Ball.posY
+                time = 0
+                angle = Ball.getImpactAngle(angle, power)
+                print(angle)
+                # angle = Ball.findAngle((prevX, prevY))
+                # print('Bounce angle is', angle)
+            else:
+                shoot = False
+        if Ball.posX >= (SCREEN_HEIGHT - Ball.radius):
+            Ball.posX = SCREEN_WIDTH - Ball.radius
+            angle = Ball.getImpactAngle(angle, power, True)
+            x = Ball.posX
+            y = Ball.posY
+            time = 0
+            power *= 0.8 #Energy lost to wall
+        elif Ball.posX <= (0 - Ball.radius):
+            Ball.posX = Ball.radius
+            angle = Ball.getImpactAngle(angle, power, True)
+            x = Ball.posX
+            y = Ball.posY
+            time = 0
+            power *= 0.9 #Energy lost to wall
+        
+        
+
+            
     clock.tick(FPS) #syncs clock up to game fps
     mouse_pos = pygame.mouse.get_pos()
     mouse_line = [(Ball.posX, Ball.posY), mouse_pos] #List with ball and mouse positions
@@ -100,7 +141,8 @@ while running:
                 time = 0
                 power = math.sqrt((mouse_line[1][1] - mouse_line[0][1])**2 + (mouse_line[1][0] - mouse_line[0][0])**2) #Pythagorous Thereom equation
                 angle = Ball.findAngle(pygame.mouse.get_pos()) #Passes in current mouse pos to find angle between it and the ball.
-    screen.fill(BLUE)
+    #screen.fill(BLUE)
+    screen.blit(yakuza_bg, (0,0))
     game_sprites.update() #updating all game sprites
     pygame.display.flip()
         
