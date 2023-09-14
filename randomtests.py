88c08aa473620347cc3617a423b2d19e376c3bf3
CharlieLinley <charlielinley15@gmail.com>
2023-09-14 12:05:45 +0100

    Fixed only being able to shoot once.
    Signed-off-by: CharlieLinley <charlielinley15@gmail.com>

--- randomtests.py
+++ randomtests.py
@@ -2,10 +2,10 @@ import pygame
 import random
 import math
 
-SCREEN_WIDTH = 800 
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 FPS = 60
-BLUE = (0,0,255)
+BLUE = (0, 0, 255)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Golf")
 
@@ -15,20 +15,22 @@ class ball(pygame.sprite.Sprite):
         self.radius = 10
         self.posX = 400
         self.posY = 790
+        self.veloX = 0
+        self.veloY = 0  # Add initial vertical velocity attribute
+        self.shooting = False
 
     def ball_path(self, startx, starty, power, ang, time):
         veloX = math.cos(ang) * power
         veloY = math.sin(ang) * power
-        print(veloX, veloY)
+
         distX = veloX * time
-        distY = (veloY * time) + (0.5 * (-300) * (time ** 2))  # Add gravity effect
+        distY = (veloY * time) + (0.5 * (9.8) * (time ** 2))  # Add gravity effect
 
         newX = round(startx + distX)
         newY = round(starty - distY)
 
         return (newX, newY)
 
-    
     def findAngle(self, pos):
         X = self.posX
         Y = self.posY
@@ -48,15 +50,11 @@ class ball(pygame.sprite.Sprite):
         
         return angle
 
-    
     def update(self):
-        pygame.draw.circle(screen, (255,255,255), (self.posX, self.posY), self.radius) #White (255,255,255) Outline to circle
-        pygame.draw.circle(screen, (0,0,0), (self.posX, self.posY), self.radius - 1) #Main Black (0,0,0) circle body
-        pygame.draw.line(screen, (255,255,255), mouse_line[0], mouse_line[1]) #Draws line between mouse and ball.
+        pygame.draw.circle(screen, (255, 255, 255), (self.posX, self.posY), self.radius)
+        pygame.draw.circle(screen, (0, 0, 0), (self.posX, self.posY), self.radius - 1)
+        pygame.draw.line(screen, (255, 255, 255), mouse_line[0], mouse_line[1])
 
-
-
-#grouping sprites for easy updating
 game_sprites = pygame.sprite.Group()
 Ball = ball()
 game_sprites.add(Ball)
@@ -68,41 +66,48 @@ power = 0
 angle = 0
 shoot = False
 
+GROUND_LEVEL = SCREEN_HEIGHT - Ball.radius
 
 '''Main Game Loop'''
-clock = pygame.time.Clock()   ## Sync fps with timer 
+clock = pygame.time.Clock()
 running = True
 while running:
-    if shoot:
-        if Ball.posY <= (SCREEN_HEIGHT - Ball.radius): #Ensures ball is not off screen.
+    if Ball.shooting:
+        if Ball.posY <= GROUND_LEVEL:
             time += 0.05
-            po = Ball.ball_path(x,y,power,angle,time)
+            po = Ball.ball_path(x, y, power, angle, time)
             Ball.posX = po[0]
             Ball.posY = po[1]
         else:
-            shoot = False
-            Ball.posY = 790
+            # Reverse the initial vertical velocity for bounce
+            Ball.veloY = -Ball.veloY
+            Ball.posY = GROUND_LEVEL
+    else:
+        Ball.shooting = False
+        Ball.posY = GROUND_LEVEL
     if Ball.posX > (SCREEN_HEIGHT - Ball.radius):
-            Ball.posX = 800
+        Ball.posX = 800
     elif Ball.posX < (0 - Ball.radius):
-            Ball.posX = 0
-    clock.tick(FPS) #syncs clock up to game fps
+        Ball.posX = 0
+    clock.tick(FPS)
     mouse_pos = pygame.mouse.get_pos()
-    mouse_line = [(Ball.posX, Ball.posY), mouse_pos] #List with ball and mouse positions
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: #When user presses the quit button
-            running = False #Ends game loop
+    mouse_line = [(Ball.posX, Ball.posY), mouse_pos]
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if shoot == False:
-                shoot = True
+            if not Ball.shooting:
+                Ball.shooting = True
                 x = Ball.posX
                 y = Ball.posY
                 time = 0
-                power = math.sqrt((mouse_line[1][1] - mouse_line[0][1])**2 + (mouse_line[1][0] - mouse_line[0][0])**2) #Pythagorous Thereom equation
-                angle = Ball.findAngle(pygame.mouse.get_pos()) #Passes in current mouse pos to find angle between it and the ball.
+                power = math.sqrt(
+                    (mouse_line[1][1] - mouse_line[0][1]) ** 2 + (mouse_line[1][0] - mouse_line[0][0]) ** 2
+                )
+                angle = Ball.findAngle(pygame.mouse.get_pos())
+                Ball.veloY = power * math.sin(angle)  # Store initial vertical velocity
     screen.fill(BLUE)
-    game_sprites.update() #updating all game sprites
+    game_sprites.update()
     pygame.display.flip()
-        
 
-pygame.quit() #Stops code, only reached once game loop has ended.
+pygame.quit()
